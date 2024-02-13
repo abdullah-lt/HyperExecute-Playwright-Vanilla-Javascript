0672d1f93d704298b44c8b2ba8a3d938cac9a606
abdullah-lt <abdullahh@lambdatest.com>
2024-02-13 20:57:30 +0000

    first

---
+++ playwright/index.jsx
@@ -0,0 +1,5 @@
+// Import styles, initialize component theme here.
+// import '../src/common.css';
+import '../sample-frontend/src/index.css';
+import '../sample-frontend/src/App.css';
+import React from 'react';
---
+++ sample-frontend/src/App.test.js
@@ -0,0 +1,11 @@
+const { expect, test } = require('@playwright/experimental-ct-react')
+
+
+import App from './App';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+  const component = await mount(<App />);
+  await expect(component).toContainText('Learn React');
+});
