f4ab122c2817a7e656bd9c2661d91d69a7b124cc
abdullah <abdullahh@lambdatest.com>
2024-02-14 20:31:42 +0530

    fixed issue

--- sample-frontend/playwright-ct.config.js
+++ sample-frontend/playwright-ct.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './__snapshots__',
   /* Maximum time one test can run for. */
-  timeout: 10 * 1000,
+  timeout: 100 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- sample-frontend/src/App.test.js
+++ sample-frontend/src/App.test.js
@@ -6,4 +6,9 @@ import App from './App';
 test('should work', async ({ mount }) => {
   const component = await mount(<App />);
   await expect(component).toContainText('Learn React');
+  await sleep(30000)
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
